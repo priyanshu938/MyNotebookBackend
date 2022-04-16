477aa0460e5681f130cf46538dce131936a760fd
Priyanshu Tiwari <you@example.com>
2022-04-16 14:06:50 +0530

    my notebook backend pushed

---
+++ db.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const mongoURI = `${process.env.MONGO_URL}`;
+const connectToMongo = () => {
+  mongoose.connect(mongoURI, () => {
+    console.log("Connected to Mongo successfully");
+  });
+};
+module.exports = connectToMongo;
