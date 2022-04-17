c34343e326eb4be1db950a3c4ea4e6ddcac7c1a9
Priyanshu_Tiwari <priyanshutiwari6789@gmail.com>
2022-04-17 13:08:39 +0530

    +1

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
-const mongoURI = `${process.env.MONGO_URL}`;
+const mongoURI = process.env.MONGO_URL;
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
     console.log("Connected to Mongo successfully");
